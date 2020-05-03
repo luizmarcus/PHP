f71a57e4271cd39c4c6402ef9d982cbc4dcc3adf
luizmarcus <luizm1000@hotmail.com>
2020-05-03 15:46:21 -0300

    Faltou uma aspa simples no codigo

--- witaitelegrambot/index.php
+++ witaitelegrambot/index.php
@@ -2,7 +2,7 @@
 
 require('parser.php');
 
-define('BOT_TOKEN', 'SEUTOKEN);
+define('BOT_TOKEN', 'SEUTOKEN');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function processMessage($message, $text) {
